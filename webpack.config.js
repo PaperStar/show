06f898bd077f2330f00848f68c4d93792256030a
YunYouJun <me@yunyoujun.cn>
2018-04-05 01:13:31 +0800

    source webslides & custom

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,6 @@
-const path = require('path');
-const webpack = require('webpack');
 const SmartBannerPlugin = require('smart-banner-webpack-plugin');
+const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const src = path.join(__dirname, 'src');
 const pkg = require('./package.json');
@@ -14,28 +11,21 @@ module.exports = {
     webslides: './js/full.js'
   },
   output: {
-    filename: './static/js/[name].js',
-    path: path.join(__dirname, './dist'),
-    publicPath: '/'
+    filename: '[name].js',
+    path: path.join(__dirname, 'static/js'),
+    publicPath: '/static/js/'
   },
   devServer: {
-    contentBase: './dist',
-    host: 'localhost',
-    hot: true
+    contentBase: __dirname,
+    host: '0.0.0.0'
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: "css-loader"
-        })
-      },
-      {
         test: /\.js$/,
         use: [
-          'babel-loader'
+          'babel-loader',
+          'eslint-loader'
         ],
         include: src
       },
@@ -50,25 +40,11 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new ExtractTextPlugin('/static/css/webslides.css'),
+    new ExtractTextPlugin('../css/webslides.css'),
     new SmartBannerPlugin({
-      banner: `Name: PaperStar-Show\nVersion: ${pkg.version}\nDate: ${new Date().toISOString().slice(0,10)}\nDescription: ${pkg.description}\nURL: ${pkg.homepage}\nBy: YunYouJun\n BaseOn: https://github.com/webslides/webslides`,
+      banner: `Name: WebSlides\nVersion: ${pkg.version}\nDate: ${new Date().toISOString().slice(0,10)}\nDescription: ${pkg.description}\nURL: ${pkg.homepage}\nCredits: @jlantunez, @LuisSacristan, @Belelros`,
       raw: false,
       entryOnly: true
-    }),
-    new HtmlWebpackPlugin({
-      filename: './index.html',
-      template: '../index.html',
-      inject: false
-    }),
-    // copy custom static assets
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, './static'),
-        to: path.resolve(__dirname, './dist/static'),
-        ignore: ['.*']
-      }
-    ])
-  ]
+    })
+  ],
 };
