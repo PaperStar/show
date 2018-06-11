548b37d86911612fc9e7dacd6e3a0d9c2ff36eab
YunYouJun <me@yunyoujun.cn>
2018-06-11 14:30:36 +0800

    Updates

---
+++ precache-manifest.aa44f5480c85d8b92044959e327040ac.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = [
+  {
+    "revision": "758167d776fb7bb7776a3b4b624d4827",
+    "url": "img/favicons/favicon.png"
+  },
+  {
+    "revision": "bffc6023835e717c0348c41583e56eba",
+    "url": "js/fontawesome-all.min.js"
+  },
+  {
+    "revision": "6d3a3fda266a7685319ff5a0c4ccb467",
+    "url": "js/webslides.min.js"
+  },
+  {
+    "revision": "bfa7911a28933ce70a8ffaba848699f8",
+    "url": "img/frame/gameFrame.png"
+  },
+  {
+    "revision": "1f5cb421da2c6ce8ce24120d3c7e0666",
+    "url": "index.html"
+  },
+  {
+    "revision": "8376fd9c49d9381afbb7bf5463215f4e",
+    "url": "img/iphone-hand.png"
+  },
+  {
+    "revision": "872b6d26fa2c0efdc8e9853a7bfbeda0",
+    "url": "css/webslides.css"
+  },
+  {
+    "revision": "6bcd473e70297a53c794019a840d7b63",
+    "url": "img/android.png"
+  },
+  {
+    "revision": "93a18ecb0be09275281a59aaef8fbc5e",
+    "url": "img/bg/sky-cloud.jpg"
+  },
+  {
+    "revision": "50d576306e2485ebb26e14276c614c51",
+    "url": "img/bt-appstore.png"
+  },
+  {
+    "revision": "6edb3470af79848b72f6ad8ca46cc957",
+    "url": "img/bt-playstore.png"
+  },
+  {
+    "revision": "f7082488a85b4ceb5e3417990314fd2b",
+    "url": "img/character/Atwood.jfif"
+  },
+  {
+    "revision": "4b270df5a2b388ea506fe21fb4319e2d",
+    "url": "img/character/zhangxiaolong.jpg"
+  },
+  {
+    "revision": "1e129a70b99a7ad97e1edb932daaeef8",
+    "url": "img/bg/ready.jpg"
+  },
+  {
+    "revision": "4f458fb6eef795c4ff405e8f412f0bd9",
+    "url": "img/davinci.png"
+  },
+  {
+    "revision": "f57f097cfca0f31ee153c122ba9b8bd0",
+    "url": "img/favicons/favicon-120.png"
+  },
+  {
+    "revision": "27145627987990012853a8f7f6bb5bd0",
+    "url": "img/bg/keyboard.jpg"
+  },
+  {
+    "revision": "03aab5e1b4e54106270e865319a08ee1",
+    "url": "img/favicons/favicon-152.png"
+  },
+  {
+    "revision": "9af1b826da2e23b8f2e4e6e1e5ab7c3c",
+    "url": "img/favicons/favicon-180.png"
+  },
+  {
+    "revision": "5db8ba4b3c0c382288f564125a96bb06",
+    "url": "img/favicons/favicon-192.png"
+  },
+  {
+    "revision": "03d2a339d00a554a958f3c9b42f3c3c9",
+    "url": "img/favicons/favicon-32.png"
+  },
+  {
+    "revision": "6141172596d95ab667f64df89128e60a",
+    "url": "img/favicons/favicon-76.png"
+  },
+  {
+    "revision": "38b4a5b3b7bdd9ee7a91c2b24f3d97aa",
+    "url": "img/swipe.svg"
+  },
+  {
+    "revision": "b9ef8c33d29e7d4add1fcbc7d7ec9547",
+    "url": "img/favicons/paper-plane-o.png"
+  },
+  {
+    "revision": "46700a5e864e6e7cae462b6edddb41fe",
+    "url": "img/logos/PaperStar.png"
+  },
+  {
+    "revision": "4820eca0718d1f294c2af9577b72c68a",
+    "url": "img/avatar.jpg"
+  },
+  {
+    "revision": "9415279a4ad294a59070a5f093e4dda6",
+    "url": "img/iphone.png"
+  },
+  {
+    "revision": "1ee53147b895a9313c1028b1780852cd",
+    "url": "img/logos/airbnb.svg"
+  },
+  {
+    "revision": "b62333ccf9c0cd765e150e40f402d232",
+    "url": "img/logos/apple.svg"
+  },
+  {
+    "revision": "d309e720e0efb7c6c6be5a1f74e0c5f9",
+    "url": "img/logos/facebook.svg"
+  },
+  {
+    "revision": "68d871416089e99a57f983f47a800884",
+    "url": "img/logos/google.svg"
+  },
+  {
+    "revision": "bbc34462047f0648d10b639be08bd84f",
+    "url": "img/logos/instagram.svg"
+  },
+  {
+    "revision": "b9ef8c33d29e7d4add1fcbc7d7ec9547",
+    "url": "img/logos/logo.png"
+  },
+  {
+    "revision": "5ed13db6c6a4e05f93e012ee095cc64c",
+    "url": "img/logos/microsoft.svg"
+  },
+  {
+    "revision": "ab19c7e070761107860dfe0c73bcdd17",
+    "url": "img/logos/netflix.svg"
+  },
+  {
+    "revision": "7d5e09f1c6a10408779c63286d8dc9a5",
+    "url": "img/logos/nyt.svg"
+  },
+  {
+    "revision": "1ab168c7d1c9735cfbeea9175f121acf",
+    "url": "img/logos/thetimes.svg"
+  },
+  {
+    "revision": "57ce5a32b8f6813812923edc325c3f6c",
+    "url": "img/qrcode/game.png"
+  },
+  {
+    "revision": "3c07a4e84fd31439f56e54866f9928a6",
+    "url": "img/qrcode/show.png"
+  },
+  {
+    "revision": "c517dc2940504b7018b37856c1f279a7",
+    "url": "img/setup.png"
+  },
+  {
+    "revision": "94f2cecdba3491eac227ace9121fee1f",
+    "url": "img/share-webslides.jpg"
+  },
+  {
+    "revision": "f6610a1d921b9a2fd2ebfc6cd0a96e1e",
+    "url": "css/fa-svg-with-js.css"
+  },
+  {
+    "revision": "d1a26434f77bdf8d7a70",
+    "url": "chunk-vendors.d1a26434.js"
+  },
+  {
+    "revision": "878b76b6da8abe526a5c",
+    "url": "app.e33bf59e.css"
+  },
+  {
+    "revision": "878b76b6da8abe526a5c",
+    "url": "app.878b76b6.js"
+  }
+];
